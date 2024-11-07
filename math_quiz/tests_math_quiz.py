82df91313f5f2e33955fe98cbbfba2e8e45a08fc
AshishMehtaAI <ashish.mehta7896@gmail.com>
2024-11-07 19:49:20 +0100

    Added files to make it pip installable

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,32 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import Random_Integer, Random_Operator, Problem_Answer_Generator
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_Random_Integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = Random_Integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_Random_Operator(self):
+        # Test if the random operator selected is always in the list of operators 
+        operators=['+', '-', '*']
+        for _ in range(1000):
+             rand_operator=Random_Operator()
+             self.assertTrue(rand_operator in operators)
 
-    def test_function_C(self):
+    def test_Problem_Answer_Generator(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (5, 2, '+', '5 + 2', 7),(3, 4, '-', '3 - 4', -1),
+                (3, 3, '-', '3 - 3', 0),(5, 2, '*', '5 * 2', 10)
             ]
-
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                 self.assertEqual((expected_problem, expected_answer),(Problem_Answer_Generator(num1, num2, operator)))
+                
 
 if __name__ == "__main__":
     unittest.main()
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:math_quiz',  # Command-line command to call the main function
+        ],
+    },
+    author="AshishMehta",
+    author_email="ashish.mehta7896@gmail.com",
+    description="A simple math quiz game",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AshishMehtaAI/DSSS_Homework_Ashish",  # Replace with your GitHub repository URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
